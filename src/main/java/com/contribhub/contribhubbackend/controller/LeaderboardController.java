9a07bc748d7931afd8d5599573bcdf1dddb7ce93
Akshay-Jogarajiya <jogarajiyaakshay@gmail.com>
2025-04-29 18:01:01 +0530

    feat:feat: add scheduler to automatically update data every 24 hours for GitHub

--- src/main/java/com/contribhub/contribhubbackend/controller/LeaderboardController.java
+++ src/main/java/com/contribhub/contribhubbackend/controller/LeaderboardController.java
@@ -13,7 +13,7 @@ import java.util.Comparator;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/leaderboard")
 public class LeaderboardController {
 
 
---
+++ src/main/java/com/contribhub/contribhubbackend/service/UpdateScheduler.java
@@ -0,0 +1,36 @@
+package com.contribhub.contribhubbackend.service;
+
+import com.contribhub.contribhubbackend.model.GitHubUser;
+import com.contribhub.contribhubbackend.repository.GitHubUserRepository;
+import com.contribhub.contribhubbackend.repository.StackOverflowUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UpdateScheduler {
+
+    @Autowired
+    GitHubService gitHubService;
+
+    @Autowired
+    StackOverflowService stackOverflowService;
+
+    @Autowired
+    GitHubUserRepository gitHubUserRepository;
+
+    @Autowired
+    StackOverflowUserRepository stackOverflowUserRepository;
+
+    @Scheduled(fixedRate = 86400000)
+    public void updateAllUserData() {
+        System.out.println("Scheduler started");
+
+        List<GitHubUser> gitHubUsers = gitHubUserRepository.findAll();
+        for (GitHubUser gitHubUser : gitHubUsers) {
+            gitHubService.fetchGitHubUserData(gitHubUser.getUsername());
+        }
+    }
+}
