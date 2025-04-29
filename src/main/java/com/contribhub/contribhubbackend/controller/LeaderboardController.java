e19a285e7b0086e7cb85a3b9a7283ff02b291fdc
Akshay-Jogarajiya <jogarajiyaakshay@gmail.com>
2025-04-29 15:10:37 +0530

    feat:add feature of a Leaderboard based on GitHub followers

---
+++ src/main/java/com/contribhub/contribhubbackend/controller/LeaderboardController.java
@@ -0,0 +1,31 @@
+package com.contribhub.contribhubbackend.controller;
+
+import com.contribhub.contribhubbackend.model.GitHubUser;
+import com.contribhub.contribhubbackend.repository.GitHubUserRepository;
+import com.contribhub.contribhubbackend.repository.StackOverflowUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class LeaderboardController {
+
+
+    @Autowired
+    GitHubUserRepository gitHubUserRepository;
+
+    @Autowired
+    StackOverflowUserRepository stackOverflowUserRepository;
+
+    @GetMapping("/github")
+    public List<GitHubUser> getGitHubLeaderboard() {
+        List<GitHubUser> users = gitHubUserRepository.findAll();
+        users.sort(Comparator.comparingInt(GitHubUser::getFollowers).reversed());
+        return users;
+    }
+}
